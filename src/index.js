fd8bf324091e8948c61216cfaadd05780f3dfca6
sudanpsg <sudanpsg@gmail.com>
2018-12-29 04:11:12 -0500

    final version

--- src/index.js
+++ src/index.js
@@ -27,8 +27,13 @@ class App extends Component {
     event.preventDefault();
     const acc = await web3.eth.getAccounts();
     console.log("inside win button");
-    if (acc[0] != this.state.manager)
+    if (acc[0] != this.state.manager) {
       this.setState({ errmsg: "sorry you are not the manager" });
+    } else {
+      this.setState({ errmsg: "picking the winner please wait" });
+      await lottery.methods.winner().send({ from: acc[0] });
+      this.setState({ ermsg: "winner is selected" });
+    }
   };
   onsubmit = async event => {
     event.preventDefault();
@@ -38,6 +43,8 @@ class App extends Component {
       from: acc[0],
       value: web3.utils.toWei(this.state.value)
     });
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({ balance });
     this.setState({ msg: "you have entered the lottery" });
   };
   async componentDidMount() {
